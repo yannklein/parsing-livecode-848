93930feb5acaea707f94dcb4f66816c17679187f
Yann Klein <yann.klein@me.com>
2022-04-16 18:35:22 +0900

    update spec storline

--- spec/scraper_spec.rb
+++ spec/scraper_spec.rb
@@ -44,7 +44,7 @@ describe "#scrape_movie" do
     expected = {
         cast: [ "Christian Bale", "Heath Ledger", "Aaron Eckhart" ],
         director: "Christopher Nolan",
-        storyline: "When the menace known as the Joker emerges from his mysterious past, he wreaks havoc and chaos on the people of Gotham. The Dark Knight must accept one of the greatest psychological and physical tests of his ability to fight injustice.",
+        storyline: "When the menace known as the Joker wreaks havoc and chaos on the people of Gotham, Batman must accept one of the greatest psychological and physical tests of his ability to fight injustice.",
         title: "The Dark Knight",
         year: 2008
       }
